c0a82ad9890e4f798eaa39b85e5470ddf2f98ad2
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2018-12-19 00:01:07 +0100

    Project 7 finished - TODO's in description
    Highest priority: 
    -pas het design van die equals onmiddellijk aan!!!
    -Philippe zorgt dat de taak op tijd geupload wordt
    
    Less important:
    -Bekijk heel het datapath nog eens, zorg dat je alles kan begrijpen, ontbreekt er iets? Laat het dan weten of pas het zelf aan (maar fuck het aub niet op)
    -Schrijf nog wat extra testen, because why not xp

--- Project 6-7/lazyTest.sh
+++ Project 6-7/lazyTest.sh
@@ -1,5 +1,7 @@
-python Test.py -f -t fibonacci.txt -c SD_Group50.circ
-#python Test.py -f -t TestBEQ.txt -c SD_Group50.circ
-#python Test.py -f -t TestJAL.txt -c SD_Group50.circ
-#python Test.py -f -t TestADDI.txt -c SD_Group50.circ
-#python Test.py -s -i SDTest_Group50.txt -c SD_Group50.circ
+#python Test.py -f -t sort.txt -c SD_Group50.circ
+#python Test.py -f -t fibonacci.txt -c SD_Group50.circ
+python Test.py -f -t TestBEQ.txt -c SD_Group50.circ
+python Test.py -f -t TestBLT.txt -c SD_Group50.circ
+python Test.py -f -t TestJAL.txt -c SD_Group50.circ
+python Test.py -f -t TestADDI.txt -c SD_Group50.circ
+python Test.py -s -i SDTest_Group50.txt -c SD_Group50.circ
