d01b1800c41b6f09e0c0171db333094406c0cfed
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2018-12-18 20:04:26 +0100

    Fibonacci + Oefening 2 + fix probleem veroorzaakt door project 4

--- Project 6-7/lazyTest.sh
+++ Project 6-7/lazyTest.sh
@@ -1,2 +1,5 @@
-python Test.py -f -t newTests.txt -c SD_Group50.circ
+python Test.py -f -t fibonacci.txt -c SD_Group50.circ
+#python Test.py -f -t TestBEQ.txt -c SD_Group50.circ
+#python Test.py -f -t TestJAL.txt -c SD_Group50.circ
+#python Test.py -f -t TestADDI.txt -c SD_Group50.circ
 #python Test.py -s -i SDTest_Group50.txt -c SD_Group50.circ
